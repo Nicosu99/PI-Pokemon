eecfc0aec76858c6a5233c150f8b63f210e3c17f
Nico <nicolasdariosuarez99@gmail.com>
2023-03-15 09:30:05 -0300

    add models

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,52 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
-};
+function pokeModel(sequelize){
+  sequelize.define(
+    'Pokemon', {
+
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hp: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      attack: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      defense: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      speed: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      height: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      created: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    }
+  )
+}
+
+module.exports = pokeModel;
---
+++ api/src/models/Type.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require ('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize
+
+function typeModel(sequelize) {
+    //Defino el modelo
+    sequelize.define(
+        'Type',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        }
+    )
+}
+
+module.exports = typeModel;
