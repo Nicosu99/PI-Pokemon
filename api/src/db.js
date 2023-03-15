759979254b02ac9356d9b7c2338a3bf130e1061b
Nico <nicolasdariosuarez99@gmail.com>
2023-03-15 10:47:39 -0300

    relacionado de bd

--- api/src/db.js
+++ api/src/db.js
@@ -39,8 +39,10 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Pokemon, Type } = sequelize.models;
 
+Pokemon.belongsToMany(Type, { through: 'Pokemon_types'})
+Type.belongsToMany(Pokemon, { through: 'Pokemon_types'})
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
