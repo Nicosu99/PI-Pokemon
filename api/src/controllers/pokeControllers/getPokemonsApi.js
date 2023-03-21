08515b7f9e0bc30fce3a934f6b7d9c8f26dae25e
Nico <nicolasdariosuarez99@gmail.com>
2023-03-20 21:15:20 -0300

    mi novia es la mas onita

---
+++ api/src/controllers/pokeControllers/getPokemonsApi.js
@@ -0,0 +1,31 @@
+const axios = require ("axios");
+const type = require("../../db");
+
+const getPokemonsApi = async (url = `https://pokeapi.com/api/v2/pokemon`) => {
+    try {
+        const resultApi = await axios.get(url);                                
+        const nextApi = await axios.get(resultApi.data.next);
+        const allPokeApi = [...resultApi.data.results, ...nextApi.data.results];
+        /* HASTA ACA TRAIGO TODOS LOS DATOS DE LA POKE-API*/
+        
+        for (const pokemon of allPokeApi){
+            const url = await axios.get(pokemon.url);
+            delete pokemon.url
+
+            pokemon.id = url.data.id;
+            pokemon.height = url.data.height;
+            pokemon.weight = url.data.weight;
+            pokemon.hp = url.data.stats[0].base_stat;
+            pokemon.attack = url.data.stats[1].base_stat;
+            pokemon.defense = url.data.stats[2].base_stat;
+            pokemon.speed = url.data.stasts[5].base_stat;
+            pokemon.types = url.data.types.map((el) => el.type.name);
+            pokemon.image = url.data.sprites.other['official-artwork'].front_default;
+            pokemon.created = false;
+            // FALTA TRAER IMAGEN EN MOVIMIENTO PARA LAS ESTADISTICAS DEL POKE
+        }
+        return allPokeApi;
+    } catch (error) {throw new Error("Error al traer pokemons de la API")}   
+};
+
+module.exports = getPokemonsApi;
